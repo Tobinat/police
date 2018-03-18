68097fd059a5e470da67e415fdc2315df7b32ff8
Tobinat <tobias.lindemann@me.com>
2018-03-18 22:32:36 +0100

    Copy

--- doc_data.php
+++ doc_data.php
@@ -148,7 +148,7 @@ if(isset($_COOKIE["LSTZ"])){
 			</div>
 			<?php require_once("sidebar.php"); ?>
 				<footer>
-					<p class="copyright">&copy; Copyright 2017 <a href="http://coltonbrister.com" target="_blank">Colton Brister</a></p>
+					<p class="copyright">&copy; Copyright 2018 <a href="http://project-rpg.de" target="_blank">PRPG</a></p>
 				</footer>
 			</div>
 		</div>
